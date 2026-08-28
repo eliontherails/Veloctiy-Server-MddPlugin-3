c507db6b6f2fc8a21fefcf5b919acc09c18aba6d
Ali-Rafay-py <writeatali2@gmail.com>
2026-08-28 15:41:59 +0500

    Dmb Plugin Server with Velocity Connections

---
+++ Bedwars/start.bat
@@ -0,0 +1,4 @@
+@echo off
+title MiniCraft - BEDWARS
+"C:\Program Files\Eclipse Adoptium\jdk-21.0.12.101-hotspot\bin\java.exe" -Xms1G -Xmx2G -jar paper.jar --nogui
+pause
---
+++ Velocity/start.bat
@@ -0,0 +1,4 @@
+@echo off
+title MiniCraft - VELOCITY PROXY
+"C:\Program Files\Eclipse Adoptium\jdk-21.0.12.101-hotspot\bin\java.exe" -Xms512M -Xmx1G -jar velocity.jar
+pause
